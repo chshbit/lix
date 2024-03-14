e2a1f79490c14607b5e1c3a523b448219970dcfa
Rebecca Turner <rbt@sent.as>
2024-03-14 11:31:22 -0700

    Add `repl-overlays`
    Adds a `repl-overlays` option, which specifies files that can overlay
    and modify the top-level bindings in `nix repl`. For example, with the
    following contents in `~/.config/nix/repl.nix`:
    
        info: final: prev: let
          optionalAttrs = predicate: attrs:
            if predicate
            then attrs
            else {};
        in
          optionalAttrs (prev ? legacyPackages && prev.legacyPackages ? ${info.currentSystem})
          {
            pkgs = prev.legacyPackages.${info.currentSystem};
          }
    
    We can run `nix repl` and use `pkgs` to refer to `legacyPackages.${currentSystem}`:
    
        $ nix repl --repl-overlays ~/.config/nix/repl.nix nixpkgs
        Nix 2.21.0pre20240309_4111bb6
        Type :? for help.
        Loading installable 'flake:nixpkgs#'...
        Added 5 variables.
        Loading 'repl-overlays'...
        Added 6 variables.
        nix-repl> pkgs.bash
        «derivation /nix/store/g08b5vkwwh0j8ic9rkmd8mpj878rk62z-bash-5.2p26.drv»
    
    Change-Id: Ic12e0f2f210b2f46e920c33088dfe1083f42391a

--- tests/functional/repl_characterization/repl_characterization.cc
+++ tests/functional/repl_characterization/repl_characterization.cc
@@ -179,9 +179,16 @@ TEST_F(ReplSessionTest, tidy)
         runReplTestPath(#name);   \
     }
 
-REPL_TEST(regression_9918);
-REPL_TEST(regression_9917);
-REPL_TEST(stack_vars);
 REPL_TEST(basic_repl);
+REPL_TEST(basic_tidied);
+REPL_TEST(regression_9917);
+REPL_TEST(regression_9918);
+REPL_TEST(regression_l145);
+REPL_TEST(repl_overlays);
+REPL_TEST(repl_overlays_compose);
+REPL_TEST(repl_overlays_destructure_without_dotdotdot_errors);
+REPL_TEST(repl_overlays_destructure_without_formals_ok);
+REPL_TEST(repl_overlays_error);
+REPL_TEST(stack_vars);
 
 }; // namespace nix
